484653fb08b9af0a216823b4d5bb7a6706494902
Simon Cruanes <simon.cruanes.2007@m4x.org>
2013-03-04 18:21:26 +0100

    add .replace operation in benchs

--- tests/benchs.ml
+++ tests/benchs.ml
@@ -24,6 +24,35 @@ let _ =
   in
   Bench.summarize 1. res
 
+let phashtbl_replace n =
+  let h = PHashtbl.create 50 in
+  for i = 0 to n do
+    PHashtbl.replace h i i;
+  done;
+  for i = 0 to n do
+    PHashtbl.replace h i i;
+  done;
+  h
+
+let hashtbl_replace n =
+  let h = Hashtbl.create 50 in
+  for i = 0 to n do
+    Hashtbl.replace h i i;
+  done;
+  for i = 0 to n do
+    Hashtbl.replace h i i;
+  done;
+  h
+
+let _ =
+  let n = 50000 in
+  let res = Bench.bench_funs
+    ["phashtbl_replace", (fun n -> ignore (phashtbl_replace n));
+     "hashtbl_replace", (fun n -> ignore (hashtbl_replace n));]
+    n
+  in
+  Bench.summarize 1. res
+
 let phashtbl_mem h =
   fun n ->
     for i = 0 to n do
