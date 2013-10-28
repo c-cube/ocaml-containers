b9d38ff62431f19ab7cc181253cc146a54634d47
Simon Cruanes <simon.cruanes.2007@m4x.org>
2013-10-28 22:27:13 +0100

    updated tests

--- tests/test_PersistentHashtbl.ml
+++ tests/test_PersistentHashtbl.ml
@@ -2,7 +2,7 @@
 open OUnit
 
 module H = PersistentHashtbl.Make(struct type t = int let equal = (=) let hash i = i end)
-open Gen.Infix
+open Sequence.Infix
 
 let test_add () =
   let h = H.create 32 in
@@ -16,29 +16,29 @@ let my_list =
     4, "d";
   ]
 
-let my_gen = Gen.of_list my_list
+let my_seq = Sequence.of_list my_list
 
-let test_of_gen () =
-  let h = H.of_gen my_gen in
+let test_of_seq () =
+  let h = H.of_seq my_seq in
   OUnit.assert_equal "b" (H.find h 2);
   OUnit.assert_equal "a" (H.find h 1);
   OUnit.assert_raises Not_found (fun () -> H.find h 42);
   ()
 
-let test_to_gen () =
-  let h = H.of_gen my_gen in
-  let l = Gen.to_list (H.to_gen h) in
+let test_to_seq () =
+  let h = H.of_seq my_seq in
+  let l = Sequence.to_list (H.to_seq h) in
   OUnit.assert_equal my_list (List.sort compare l)
 
 let test_resize () =
-  let h = H.of_gen
-    (Gen.map (fun i -> i, string_of_int i)
-      (Gen.int_range 0 200)) in
+  let h = H.of_seq
+    Sequence.(map (fun i -> i, string_of_int i)
+      (0 -- 200)) in
   OUnit.assert_equal 201 (H.length h);
   ()
 
 let test_persistent () =
-  let h = H.of_gen my_gen in
+  let h = H.of_seq my_seq in
   OUnit.assert_equal "a" (H.find h 1);
   OUnit.assert_raises Not_found (fun () -> H.find h 5);
   let h' = H.replace h 5 "e" in
@@ -50,25 +50,25 @@ let test_persistent () =
 
 let test_big () =
   let n = 10000 in
-  let gen = Gen.map (fun i -> i, string_of_int i) (0--n) in
-  let h = H.of_gen gen in
+  let seq = Sequence.map (fun i -> i, string_of_int i) (0--n) in
+  let h = H.of_seq seq in
   (*
-  Format.printf "@[<v2>table:%a@]@." (Gen.pp_gen
+  Format.printf "@[<v2>table:%a@]@." (Sequence.pp_seq
     (fun formatter (k,v) -> Format.fprintf formatter "%d -> \"%s\"" k v))
-    (H.to_gen h);
+    (H.to_seq h);
   *)
-  Gen.iter
+  Sequence.iter
     (fun (k,v) ->
       (*
       Format.printf "lookup %d@." k;
       *)
       OUnit.assert_equal ~printer:(fun x -> x) v (H.find h k))
-    gen;
+    seq;
   OUnit.assert_raises Not_found (fun () -> H.find h (n+1));
   ()
 
 let test_remove () =
-  let h = H.of_gen my_gen in
+  let h = H.of_seq my_seq in
   OUnit.assert_equal (H.find h 2) "b";
   OUnit.assert_equal (H.find h 3) "c";
   OUnit.assert_equal (H.find h 4) "d";
@@ -80,12 +80,12 @@ let test_remove () =
   OUnit.assert_raises Not_found (fun () -> H.find h 2)
 
 let test_size () =
-  let open Gen.Infix in
+  let open Sequence.Infix in
   let n = 10000 in
-  let gen = Gen.map (fun i -> i, string_of_int i) (0 -- n) in
-  let h = H.of_gen gen in
+  let seq = Sequence.map (fun i -> i, string_of_int i) (0 -- n) in
+  let h = H.of_seq seq in
   OUnit.assert_equal (n+1) (H.length h);
-  let h = Gen.fold (fun h i -> H.remove h i) h (0 -- 500) in
+  let h = Sequence.fold (fun h i -> H.remove h i) h (0 -- 500) in
   OUnit.assert_equal (n-500) (H.length h);
   OUnit.assert_bool "is_empty" (H.is_empty (H.create 16));
   ()
@@ -93,8 +93,8 @@ let test_size () =
 let suite =
   "test_H" >:::
     [ "test_add" >:: test_add;
-      "test_of_gen" >:: test_of_gen;
-      "test_to_gen" >:: test_to_gen;
+      "test_of_seq" >:: test_of_seq;
+      "test_to_seq" >:: test_to_seq;
       "test_resize" >:: test_resize;
       "test_persistent" >:: test_persistent;
       "test_big" >:: test_big;
