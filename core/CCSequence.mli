d638038089a5d15c4c67846680ce2ba447acbd08
Simon Cruanes <simon.cruanes.2007@m4x.org>
2014-08-08 20:26:04 +0200

    CCSequence now shares code with the 'sequence' library; merged the .mli so it reflects versions properly

--- core/CCSequence.mli
+++ core/CCSequence.mli
@@ -23,16 +23,16 @@ OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
 OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 *)
 
-(** {1 Transient iterators, that abstract on a finite sequence of elements.} *)
+(** {1 Simple and Efficient Iterators} *)
 
 (** The iterators are designed to allow easy transfer (mappings) between data
-    structures, without defining n^2 conversions between the n types. The
+    structures, without defining [n^2] conversions between the [n] types. The
     implementation relies on the assumption that a sequence can be iterated
     on as many times as needed; this choice allows for high performance
     of many combinators. However, for transient iterators, the {!persistent}
     function is provided, storing elements of a transient iterator
     in memory; the iterator can then be used several times (See further).
-    
+
     Note that some combinators also return sequences (e.g. {!group}). The
     transformation is computed on the fly every time one iterates over
     the resulting sequence. If a transformation performs heavy computation,
@@ -42,7 +42,7 @@ OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
     until their result is iterated on. For instance, if one calls {!map}
     on a sequence, one gets a new sequence, but nothing else happens until
     this new sequence is used (by folding or iterating on it).
-    
+
     If a sequence is built from an iteration function that is {b repeatable}
     (i.e. calling it several times always iterates on the same set of
     elements, for instance List.iter or Map.iter), then
@@ -53,8 +53,8 @@ OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
     of this memory structure, cheaply and repeatably. *)
 
 type +'a t = ('a -> unit) -> unit
-  (** Sequence iterator type, representing a finite sequence of values
-      of type ['a] that one can iterate on. *)
+  (** A sequence of values of type ['a]. If you give it a function ['a -> unit]
+      it will be applied to every element of the sequence successively. *)
 
 type +'a sequence = 'a t
 
@@ -76,12 +76,34 @@ val empty : 'a t
 val singleton : 'a -> 'a t
   (** Singleton sequence, with exactly one element. *)
 
+val doubleton : 'a -> 'a -> 'a t
+  (** Sequence with exactly two elements
+      @since NEXT_RELEASE *)
+
+val cons : 'a -> 'a t -> 'a t
+  (** [cons x l] yields [x], then yields from [l].
+      Same as [append (singleton x) l]
+      @since NEXT_RELEASE *)
+
+
+val snoc : 'a t -> 'a -> 'a t
+  (** Same as {!cons} but yields the element after iterating on [l]
+      @since NEXT_RELEASE *)
+
+val return : 'a -> 'a t
+  (** Synonym to {!singleton}
+      @since NEXT_RELEASE *)
+
+val pure : 'a -> 'a t
+  (** Synonym to {!singleton}
+      @since NEXT_RELEASE *)
+
 val repeat : 'a -> 'a t
   (** Infinite sequence of the same element. You may want to look
-      at {!take} if you iterate on it. *)
+      at {!take} and the likes if you iterate on it. *)
 
 val iterate : ('a -> 'a) -> 'a -> 'a t
-  (** [iterate f x] is the infinite sequence (x, f(x), f(f(x)), ...) *)
+  (** [iterate f x] is the infinite sequence [x, f(x), f(f(x)), ...] *)
 
 val forever : (unit -> 'b) -> 'b t
   (** Sequence that calls the given function to produce elements.
@@ -97,7 +119,8 @@ val cycle : 'a t -> 'a t
 (** {2 Consume a sequence} *)
 
 val iter : ('a -> unit) -> 'a t -> unit
-  (** Consume the sequence, passing all its arguments to the function *)
+  (** Consume the sequence, passing all its arguments to the function.
+      Basically [iter f seq] is just [seq f]. *)
 
 val iteri : (int -> 'a -> unit) -> 'a t -> unit
   (** Iterate on elements and their index in the sequence *)
@@ -119,6 +142,15 @@ val for_all : ('a -> bool) -> 'a t -> bool
 
 val exists : ('a -> bool) -> 'a t -> bool
   (** Exists there some element satisfying the predicate? *)
+
+val mem : ?eq:('a -> 'a -> bool) -> 'a -> 'a t -> bool
+  (** Is the value a member of the sequence?
+      @param eq the equality predicate to use (default [(=)])
+      @since NEXT_RELEASE *)
+
+val find : ('a -> 'b option) -> 'a t -> 'b option
+  (** Find the first element on which the function doesn't return [None]
+      @since NEXT_RELEASE *)
 
 val length : 'a t -> int
   (** How long is the sequence? Forces the sequence. *)
@@ -145,17 +177,42 @@ val flatMap : ('a -> 'b t) -> 'a t -> 'b t
   (** Monadic bind. Intuitively, it applies the function to every element of the
       initial sequence, and calls {!concat}. *)
 
+val flat_map : ('a -> 'b t) -> 'a t -> 'b t
+  (** Alias to {!flatMap} with a more explicit name
+      @since NEXT_RELEASE *)
+
 val fmap : ('a -> 'b option) -> 'a t -> 'b t
   (** Specialized version of {!flatMap} for options.  *)
+
+val filter_map : ('a -> 'b option) -> 'a t -> 'b t
+  (** Alias to {!fmap} with a more explicit name
+      @since NEXT_RELEASE *)
 
 val intersperse : 'a -> 'a t -> 'a t
   (** Insert the single element between every element of the sequence *)
 
+(** {2 Caching} *)
+
 val persistent : 'a t -> 'a t
-  (** Iterate on the sequence, storing elements in a data structure.
+  (** Iterate on the sequence, storing elements in an efficient internal structure..
       The resulting sequence can be iterated on as many times as needed.
       {b Note}: calling persistent on an already persistent sequence
       will still make a new copy of the sequence! *)
+
+val persistent_lazy : 'a t -> 'a t
+  (** Lazy version of {!persistent}. When calling [persistent_lazy s],
+      a new sequence [s'] is immediately returned (without actually consuming
+      [s]) in constant time; the first time [s'] is iterated on,
+      it also consumes [s] and caches its content into a inner data
+      structure that will back [s'] for future iterations.
+
+      {b warning}: on the first traversal of [s'], if the traversal
+      is interrupted prematurely ({!take}, etc.) then [s'] will not be
+      memorized, and the next call to [s'] will traverse [s] again.
+
+      @since NEXT_RELEASE *)
+
+(** {2 Misc} *)
 
 val sort : ?cmp:('a -> 'a -> int) -> 'a t -> 'a t
   (** Sort the sequence. Eager, O(n) ram and O(n ln(n)) time.
@@ -173,9 +230,14 @@ val uniq : ?eq:('a -> 'a -> bool) -> 'a t -> 'a t
       like [fun seq -> map List.hd (group seq)]. *)
 
 val product : 'a t -> 'b t -> ('a * 'b) t
-  (** Cartesian product of the sequences. The first one is transformed
-      by calling [persistent] on it, so that it can be traversed
-      several times (outer loop of the product) *)
+  (** Cartesian product of the sequences. When calling [product a b],
+      the caller {b MUST} ensure that [b] can be traversed as many times
+      as required (several times), possibly by calling {!persistent} on it
+      beforehand. *)
+
+val product2 : 'a t -> 'b t -> ('a, 'b) t2
+  (** Binary version of {!product}. Same requirements.
+      @since NEXT_RELEASE *)
 
 val join : join_row:('a -> 'b -> 'c option) -> 'a t -> 'b t -> 'c t
   (** [join ~join_row a b] combines every element of [a] with every
@@ -183,7 +245,7 @@ val join : join_row:('a -> 'b -> 'c option) -> 'a t -> 'b t -> 'c t
       the two elements do not combine. Assume that [b] allows for multiple
       iterations. *)
 
-val unfoldr : ('b -> ('a * 'b) option) -> 'b -> 'a t 
+val unfoldr : ('b -> ('a * 'b) option) -> 'b -> 'a t
   (** [unfoldr f b] will apply [f] to [b]. If it
       yields [Some (x,b')] then [x] is returned
       and unfoldr recurses with [b']. *)
@@ -200,12 +262,31 @@ val min : ?lt:('a -> 'a -> bool) -> 'a t -> 'a option
   (** Min element of the sequence, using the given comparison function.
       see {!max} for more details. *)
 
+val head : 'a t -> 'a option
+  (** First element, if any, otherwise [None]
+      @since NEXT_RELEASE *)
+
+val head_exn : 'a t -> 'a
+  (** First element, if any, fails
+      @raise Invalid_argument if the sequence is empty
+      @since NEXT_RELEASE *)
+
 val take : int -> 'a t -> 'a t
   (** Take at most [n] elements from the sequence. Works on infinite
       sequences. *)
 
+val take_while : ('a -> bool) -> 'a t -> 'a t
+  (** Take elements while they satisfy the predicate, then stops iterating.
+      Will work on an infinite sequence [s] if the predicate is false for at
+      least one element of [s].
+      @since NEXT_RELEASE *)
+
 val drop : int -> 'a t -> 'a t
   (** Drop the [n] first elements of the sequence. Lazy. *)
+
+val drop_while : ('a -> bool) -> 'a t -> 'a t
+  (** Predicate version of {!drop}
+      @since NEXT_RELEASE *)
 
 val rev : 'a t -> 'a t
   (** Reverse the sequence. O(n) memory and time, needs the
@@ -239,15 +320,22 @@ val map2_2 : ('a -> 'b -> 'c) -> ('a -> 'b -> 'd) -> ('a, 'b) t2 -> ('c, 'd) t2
 (** {2 Basic data structures converters} *)
 
 val to_list : 'a t -> 'a list
+  (** Convert the sequence into a list. Preserves order of elements.
+      This function is tail-recursive, but consumes 2*n memory.
+      If order doesn't matter to you, consider {!to_rev_list}. *)
 
 val to_rev_list : 'a t -> 'a list
   (** Get the list of the reversed sequence (more efficient than {!to_list}) *)
 
 val of_list : 'a list -> 'a t
 
+val to_opt : 'a t -> 'a option
+  (** Alias to {!head}
+      @since NEXT_RELEASE *)
+
 val to_array : 'a t -> 'a array
   (** Convert to an array. Currently not very efficient because
-      and intermediate list is used. *)
+      an intermediate list is used. *)
 
 val of_array : 'a array -> 'a t
 
@@ -259,6 +347,10 @@ val of_array2 : 'a array -> (int, 'a) t2
 val array_slice : 'a array -> int -> int -> 'a t
   (** [array_slice a i j] Sequence of elements whose indexes range
       from [i] to [j] *)
+
+val of_opt : 'a option -> 'a t
+  (** Iterate on 0 or 1 values.
+      @since NEXT_RELEASE *)
 
 val of_stream : 'a Stream.t -> 'a t
   (** Sequence of elements of a stream (usable only once) *)
@@ -304,10 +396,20 @@ val hashtbl_values : ('a, 'b) Hashtbl.t -> 'b t
 val of_str : string -> char t
 val to_str :  char t -> string
 
+val concat_str : string t -> string
+  (** Concatenate strings together, eagerly.
+      Also see {!intersperse} to add a separator.
+      @since NEXT_RELEASE *)
+
+exception OneShotSequence
+  (** Raised when the user tries to iterate several times on
+      a transient iterator *)
+
 val of_in_channel : in_channel -> char t
   (** Iterates on characters of the input (can block when one
       iterates over the sequence). If you need to iterate
-      several times on this sequence, use {!persistent}. *)
+      several times on this sequence, use {!persistent}.
+      @raise OneShotSequence when used more than once. *)
 
 val to_buffer : char t -> Buffer.t -> unit
   (** Copy content of the sequence into the buffer *)
@@ -327,12 +429,19 @@ val to_set : (module Set.S with type elt = 'a and type t = 'b) -> 'a t -> 'b
   (** Convert the sequence to a set, given the proper set module *)
 
 type 'a gen = unit -> 'a option
+type 'a klist = unit -> [`Nil | `Cons of 'a * 'a klist]
 
 val of_gen : 'a gen -> 'a t
   (** Traverse eagerly the generator and build a sequence from it *)
 
 val to_gen : 'a t -> 'a gen
   (** Make the sequence persistent (O(n)) and then iterate on it. Eager. *)
+
+val of_klist : 'a klist -> 'a t
+  (** Iterate on the lazy list *)
+
+val to_klist : 'a t -> 'a klist
+  (** Make the sequence persistent and then iterate on it. Eager. *)
 
 (** {2 Functorial conversions between sets and sequences} *)
 
@@ -341,11 +450,17 @@ module Set : sig
     include Set.S
     val of_seq : elt sequence -> t
     val to_seq : t -> elt sequence
+
+    val to_list : t -> elt list
+      (** @since NEXT_RELEASE *)
+
+    val of_list : elt list -> t
+      (** @since NEXT_RELEASE *)
   end
 
   (** Create an enriched Set module from the given one *)
   module Adapt(X : Set.S) : S with type elt = X.elt and type t = X.t
-    
+
   (** Functor to build an extended Set module from an ordered type *)
   module Make(X : Set.OrderedType) : S with type elt = X.t
 end
@@ -359,6 +474,12 @@ module Map : sig
     val of_seq : (key * 'a) sequence -> 'a t
     val keys : 'a t -> key sequence
     val values : 'a t -> 'a sequence
+
+    val to_list : 'a t -> (key * 'a) list
+      (** @since NEXT_RELEASE *)
+
+    val of_list : (key * 'a) list -> 'a t
+      (** @since NEXT_RELEASE *)
   end
 
   (** Adapt a pre-existing Map module to make it sequence-aware *)
@@ -390,26 +511,95 @@ val random_list : 'a list -> 'a t
 
 module Infix : sig
   val (--) : int -> int -> int t
+    (** [a -- b] is the range of integers from [a] to [b], both included,
+        in increasing order. It will therefore be empty if [a > b]. *)
 
   val (--^) : int -> int -> int t
     (** [a --^ b] is the range of integers from [b] to [a], both included,
         in decreasing order (starts from [a]).
         It will therefore be empty if [a < b]. *)
+
+  val (>>=) : 'a t -> ('a -> 'b t) -> 'b t
+    (** Monadic bind (infix version of {!flat_map}
+        @since NEXT_RELEASE *)
+
+  val (>|=) : 'a t -> ('a -> 'b) -> 'b t
+    (** Infix version of {!map}
+        @since NEXT_RELEASE *)
+
+  val (<*>) : ('a -> 'b) t -> 'a t -> 'b t
+    (** Applicative operator (product+application)
+        @since NEXT_RELEASE *)
+
+  val (<+>) : 'a t -> 'a t -> 'a t
+    (** Concatenation of sequences
+        @since NEXT_RELEASE *)
 end
 
 include module type of Infix
 
+
 (** {2 Pretty printing of sequences} *)
 
-val print : ?start:string -> ?stop:string -> ?sep:string ->
-            (Format.formatter -> 'a -> unit) ->
+val pp_seq : ?sep:string -> (Format.formatter -> 'a -> unit) ->
              Format.formatter -> 'a t -> unit
   (** Pretty print a sequence of ['a], using the given pretty printer
       to print each elements. An optional separator string can be provided. *)
 
-val pp : ?start:string -> ?stop:string -> ?sep:string ->
-         (Buffer.t -> 'a -> unit) ->
-          Buffer.t -> 'a t -> unit
+val pp_buf : ?sep:string -> (Buffer.t -> 'a -> unit) ->
+             Buffer.t -> 'a t -> unit
+  (** Print into a buffer *)
 
-val to_string : ?start:string -> ?stop:string -> ?sep:string ->
-                (Buffer.t -> 'a -> unit) -> 'a t -> string
+val to_string : ?sep:string -> ('a -> string) -> 'a t -> string
+  (** Print into a string *)
+
+(** {2 Basic IO}
+
+Very basic interface to manipulate files as sequence of chunks/lines. The
+sequences take care of opening and closing files properly; every time
+one iterates over a sequence, the file is opened/closed again.
+
+Example: copy a file ["a"] into file ["b"], removing blank lines:
+
+{[
+  Sequence.(IO.lines_of "a" |> filter (fun l-> l<> "") |> IO.write_lines "b");;
+]}
+
+By chunks of [4096] bytes:
+
+{[
+  Sequence.IO.(chunks_of ~size:4096 "a" |> write_to "b");;
+]}
+
+@since NEXT_RELEASE *)
+
+module IO : sig
+  val lines_of : ?mode:int -> ?flags:open_flag list ->
+                string -> string t
+  (** [lines_of filename] reads all lines of the given file. It raises the
+      same exception as would opening the file and read from it, except
+      from [End_of_file] (which is caught). The file is {b always} properly
+      closed.
+      Every time the sequence is iterated on, the file is opened again, so
+      different iterations might return different results
+      @param mode default [0o644]
+      @param flags default: [[Open_rdonly]] *)
+
+  val chunks_of : ?mode:int -> ?flags:open_flag list -> ?size:int ->
+                  string -> string t
+  (** Read chunks of the given [size] from the file. The last chunk might be
+      smaller. Behaves like {!lines_of} regarding errors and options.
+      Every time the sequence is iterated on, the file is opened again, so
+      different iterations might return different results *)
+
+  val write_to : ?mode:int -> ?flags:open_flag list ->
+                 string -> string t -> unit
+  (** [write_to filename seq] writes all strings from [seq] into the given
+      file. It takes care of opening and closing the file.
+      @param mode default [0o644]
+      @param flags used by [open_out_gen]. Default: [[Open_creat;Open_wronly]]. *)
+
+  val write_lines : ?mode:int -> ?flags:open_flag list ->
+                    string -> string t -> unit
+  (** Same as {!write_to}, but intercales ['\n'] between each string *)
+end
