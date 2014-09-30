df838482ef9ddc990de3bb82deefb07a610012e2
Simon Cruanes <simon.cruanes.2007@m4x.org>
2014-09-30 16:14:16 +0200

    moved Sexp into core/CCSexp

--- examples/id_sexp.ml
+++ examples/id_sexp.ml
@@ -3,11 +3,11 @@
 let () =
   if Array.length Sys.argv <> 2 then failwith "usage: id_sexp file";
   let f = Sys.argv.(1) in
-  let s = Sexp.L.of_file f in
+  let s = CCSexp.L.of_file f in
   match s with
   | `Ok l ->
       List.iter
-        (fun s -> Format.printf "@[%a@]@." Sexp.print s)
+        (fun s -> Format.printf "@[%a@]@." CCSexp.print s)
         l
   | `Error msg ->
       Format.printf "error: %s@." msg
