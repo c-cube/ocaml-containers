feec3bf46fba2c182eaf3218ca59c8e08b694dc3
Simon Cruanes <simon.cruanes.2007@m4x.org>
2015-01-25 23:08:31 +0100

    refactored heavily CCFuture (much simpler, cleaner, basic API and thread pool)

--- src/threads/CCFuture.mli
+++ src/threads/CCFuture.mli
@@ -25,147 +25,114 @@ OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
 (** {1 Futures for concurrency} *)
 
+type 'a state =
+  | Done of 'a
+  | Waiting
+  | Failed of exn
+
 type 'a t
-  (** A future value of type 'a *)
+(** A future value of type 'a *)
 
-exception SendTwice
-  (** Exception raised when a future is evaluated several time *)
+type 'a future = 'a t
 
-(** {2 MVar: a zero-or-one element thread-safe box} *)
+(** {2 Constructors} *)
 
-module MVar : sig
-  type 'a t
+val return : 'a -> 'a t
+(** Future that is already computed *)
 
-  val empty : unit -> 'a t
-    (** Create an empty box *)
+val fail : exn -> 'a t
+(** Future that fails immediately *)
 
-  val full : 'a -> 'a t
-    (** Create a full box *)
+val make : (unit -> 'a) -> 'a t
+(** Create a future, representing a value that will be computed by
+      the function. If the function raises, the future will fail. *)
 
-  val is_empty : _ t -> bool
-    (** Is the box currently empty? *)
+val make1 : ('a -> 'b) -> 'a -> 'b t
+val make2 : ('a -> 'b -> 'c) -> 'a -> 'b -> 'c t
 
-  val take : 'a t -> 'a
-    (** Take value out of the box. Wait if necessary *)
-
-  val put : 'a t -> 'a -> unit
-    (** Put a value in the box. Waits if the box is already empty *)
-
-  val update : 'a t -> ('a -> 'a) -> 'a * 'a
-    (** Use given function to atomically update content, and return
-        the previous value and the new one *)
-
-  val peek : 'a t -> 'a
-    (** Look at the value, without removing it *)
-end
-
-(** {2 Thread pool} *)
-module Pool : sig
-  type t
-    (** A pool of threads *)
-
-  val create : ?timeout:float -> size:int -> t
-    (** Create a pool with at most the given number of threads. [timeout]
-        is the time after which idle threads are killed. *)
-
-  val size : t -> int
-    (** Current size of the pool *)
-
-  val run : t -> (unit -> unit) -> unit
-    (** Run the function in the pool *)
-
-  val finish : t -> unit
-    (** Kill threads in the pool *)
-end
-
-val default_pool : Pool.t
-  (** Pool of threads that is used by default. Growable if needed. *)
-
-(** {2 Basic low-level Future functions} *)
-
-val make : Pool.t -> 'a t
-  (** Create a future, representing a value that is not known yet. *)
+(** {2 Basics} *)
 
 val get : 'a t -> 'a
-  (** Blocking get: wait for the future to be evaluated, and get the value,
-      or the exception that failed the future is returned *)
+(** Blocking get: wait for the future to be evaluated, and get the value,
+    or the exception that failed the future is returned.
+    @raise e if the exception failed with e *)
 
-val send : 'a t -> 'a -> unit
-  (** Send a result to the future. Will raise SendTwice if [send] has
-      already been called on this future before *)
-
-val fail : 'a t -> exn -> unit
-  (** Fail the future by raising an exception inside it *)
+val state : 'a t -> 'a state
+(** State of the future *)
 
 val is_done : 'a t -> bool
-  (** Is the future evaluated (success/failure)? *)
+(** Is the future evaluated (success/failure)? *)
 
 (** {2 Combinators} *)
 
 val on_success : 'a t -> ('a -> unit) -> unit
-  (** Attach a handler to be called upon success *)
+(** Attach a handler to be called upon success *)
 
 val on_failure : _ t -> (exn -> unit) -> unit
-  (** Attach a handler to be called upon failure *)
+(** Attach a handler to be called upon failure *)
 
-val on_finish : _ t -> (unit -> unit) -> unit
-  (** Attach a handler to be called when the future is evaluated *)
+val on_finish : 'a t -> ('a state -> unit) -> unit
+(** Attach a handler to be called when the future is evaluated *)
 
-val flatMap : ?pool:Pool.t -> ('a -> 'b t) -> 'a t -> 'b t
-  (** Monadic combination of futures *)
+val flat_map : ('a -> 'b t) -> 'a t -> 'b t
+(** Monadic combination of futures *)
 
-val andThen : ?pool:Pool.t -> 'a t -> (unit -> 'b t) -> 'b t
-  (** Wait for the first future to succeed, then launch the second *)
+val and_then : 'a t -> (unit -> 'b t) -> 'b t
+(** Wait for the first future to succeed, then launch the second *)
 
-val sequence : ?pool:Pool.t -> 'a t list -> 'a list t
-  (** Future that waits for all previous sequences to terminate *)
+val sequence : 'a t list -> 'a list t
+(** Future that waits for all previous sequences to terminate. If any future
+    in the list fails, [sequence l] fails too. *)
 
-val choose : ?pool:Pool.t -> 'a t list -> 'a t
-  (** Choose among those futures (the first to terminate) *)
+val choose : 'a t list -> 'a t
+(** Choose among those futures (the first to terminate). Behaves like
+    the first future that terminates, by failing if the future fails *)
 
-val map : ?pool:Pool.t -> ('a -> 'b) -> 'a t -> 'b t
-  (** Maps the value inside the future *)
+val map : ('a -> 'b) -> 'a t -> 'b t
+(** Maps the value inside the future. The function doesn't run in its
+    own task; if it can take time, use {!flat_map} *)
 
-(** {2 Future constructors} *)
+(** {2 Helpers} *)
 
-val return : 'a -> 'a t
-  (** Future that is already computed *)
-
-val spawn : ?pool:Pool.t -> (unit -> 'a) -> 'a t
-  (** Spawn a thread that wraps the given computation *)
-
-val spawn_process : ?pool:Pool.t -> ?stdin:string -> cmd:string ->
+val spawn_process : ?stdin:string -> cmd:string -> unit ->
                     (int * string * string) t
-  (** Spawn a sub-process with the given command [cmd] (and possibly input);
+(** Spawn a sub-process with the given command [cmd] (and possibly input);
       returns a future containing (returncode, stdout, stderr) *)
 
-val sleep : ?pool:Pool.t -> float -> unit t
-  (** Future that returns with success in the given amount of seconds *)
+val sleep : float -> unit t
+(** Future that returns with success in the given amount of seconds *)
 
 (** {2 Event timer} *)
 
 module Timer : sig
   type t
-    (** A scheduler for events *)
+  (** A scheduler for events. It runs in its own thread. *)
 
-  val create : ?pool:Pool.t -> unit -> t
-    (** A timer that runs tasks in the given thread pool *)
+  val create : unit -> t
+  (** A new timer. *)
 
-  val schedule_at : t -> float -> (unit -> unit) -> unit
-    (** [schedule_at s t act] will run [act] at the Unix echo [t] *)
+  val after : t -> float -> unit future
+  (** Create a future that waits for the given number of seconds, then
+      awakens with [()] *)
 
-  val schedule_in : t -> float -> (unit -> unit) -> unit
-    (** [schedule_in s d act] will run [act] in [d] seconds *)
+  val at : t -> float -> unit future
+  (** Create a future that evaluates to [()] at the given Unix timestamp *)
 
   val stop : t -> unit
-    (** Stop the given timer, cancelling pending tasks *)
+  (** Stop the given timer, cancelling pending tasks *)
 end
-
 
 module Infix : sig
   val (>>=) : 'a t -> ('a -> 'b t) -> 'b t
   val (>>) : 'a t -> (unit -> 'b t) -> 'b t
+  val (>|=) : 'a t -> ('a -> 'b) -> 'b t
 end
 
 val (>>=) : 'a t -> ('a -> 'b t) -> 'b t
 val (>>) : 'a t -> (unit -> 'b t) -> 'b t
+val (>|=) : 'a t -> ('a -> 'b) -> 'b t
+
+(** {2 Low level} *)
+
+val stop_pool : unit -> unit
+(** Stop the thread pool *)
