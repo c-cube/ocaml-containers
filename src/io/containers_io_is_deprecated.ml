b11a98c6d3f1f8e15b9f29dde4ff0fc615e969c3
Simon Cruanes <simon.cruanes.2007@m4x.org>
2015-06-29 09:55:27 +0200

    fix build issues with dummy module in `containers.io`

---
+++ src/io/containers_io_is_deprecated.ml
@@ -0,0 +1,7 @@
+(** {!CCIO} has moved into {!Containers}, the main library.
+ 
+  The reason is that it has no additional dependency and is arguably a
+  useful completement to parts of {!Pervasives} (the channel management)
+
+  As a result, linking "containers" rather than "containers.io" should be
+  enough if one needs {!CCIO}. *)
