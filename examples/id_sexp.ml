07382c02dd1e34bcea71fdf6e77d246dc15db9e9
Simon Cruanes <simon.cruanes.2007@m4x.org>
2015-12-22 10:26:56 +0100

    remove deprecated `CCSexpStream` module

--- examples/id_sexp.ml
+++ examples/id_sexp.ml
@@ -3,11 +3,11 @@
 let () =
   if Array.length Sys.argv <> 2 then failwith "usage: id_sexp file";
   let f = Sys.argv.(1) in
-  let s = CCSexpStream.L.of_file f in
+  let s = CCSexpM.parse_file_list f in
   match s with
   | `Ok l ->
       List.iter
-        (fun s -> Format.printf "@[%a@]@." CCSexpStream.print s)
+        (fun s -> Format.printf "@[%a@]@." CCSexpM.print s)
         l
   | `Error msg ->
       Format.printf "error: %s@." msg
