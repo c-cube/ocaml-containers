db67b19fe8f4f90cc78a0ef9c637dd4f8afcf58f
Simon Cruanes <simon.cruanes.2007@m4x.org>
2016-02-18 12:12:36 +0100

    update some headers, formatting

--- src/core/CCMap.mli
+++ src/core/CCMap.mli
@@ -1,32 +1,10 @@
-(*
-copyright (c) 2013-2014, simon cruanes
-all rights reserved.
 
-redistribution and use in source and binary forms, with or without
-modification, are permitted provided that the following conditions are met:
-
-redistributions of source code must retain the above copyright notice, this
-list of conditions and the following disclaimer.  redistributions in binary
-form must reproduce the above copyright notice, this list of conditions and the
-following disclaimer in the documentation and/or other materials provided with
-the distribution.
-
-THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
-ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
-WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
-DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
-FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
-DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
-SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
-CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
-OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
-OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-*)
+(* This file is free software, part of containers. See file "license" for more details. *)
 
 (** {1 Extensions of Standard Map}
 
-Provide useful functions and iterators on [Map.S]
-@since 0.5 *)
+    Provide useful functions and iterators on [Map.S]
+    @since 0.5 *)
 
 type 'a sequence = ('a -> unit) -> unit
 type 'a printer = Buffer.t -> 'a -> unit
@@ -66,13 +44,15 @@ module type S = sig
 
   val to_list : 'a t -> (key * 'a) list
 
-  val pp : ?start:string -> ?stop:string -> ?arrow:string -> ?sep:string ->
-            key printer -> 'a printer -> 'a t printer
+  val pp :
+    ?start:string -> ?stop:string -> ?arrow:string -> ?sep:string ->
+    key printer -> 'a printer -> 'a t printer
 
-  val print : ?start:string -> ?stop:string -> ?arrow:string -> ?sep:string ->
-              key formatter -> 'a formatter -> 'a t formatter
+  val print :
+    ?start:string -> ?stop:string -> ?arrow:string -> ?sep:string ->
+    key formatter -> 'a formatter -> 'a t formatter
 end
 
 module Make(O : Map.OrderedType) : S
   with type 'a t = 'a Map.Make(O).t
-  and type key = O.t
+   and type key = O.t
