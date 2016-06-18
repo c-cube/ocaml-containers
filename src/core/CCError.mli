c573f447bc2ea22230bb8336935cd300f7b7f9cf
Simon Cruanes <simon.cruanes.2007@m4x.org>
2016-06-19 01:02:06 +0200

    add more generic printers for CCError and CCResult (close #73)

--- src/core/CCError.mli
+++ src/core/CCError.mli
@@ -190,7 +190,15 @@ val to_seq : ('a, _) t -> 'a sequence
 
 val pp : 'a printer -> ('a, string) t printer
 
+val pp': 'a printer -> 'e printer -> ('a, 'e) t printer
+(** Printer that is generic on the error type
+    @since NEXT_RELEASE *)
+
 val print : 'a formatter -> ('a, string) t formatter
+
+val print' : 'a formatter -> 'e formatter -> ('a, 'e) t formatter
+(** Printer that is generic on the error type
+    @since NEXT_RELEASE *)
 
 (** {2 Global Exception Printers}
 
