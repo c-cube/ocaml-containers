d1237f1f2939a1c48d8e6f3ce2835faf23b544ce
Simon Cruanes <simon.cruanes.2007@m4x.org>
2016-07-11 23:15:48 +0200

    containers-top printers

--- src/top/containers_top.ml
+++ src/top/containers_top.ml
@@ -15,13 +15,15 @@ let install_printer s =
     ()
 let install_printers = List.iter install_printer
 
-let pp_vector pp_x out (v: _ CCVector.vector) = CCVector.print pp_x out v
+let pp_rw_vector pp_x out (v: _ CCVector.vector) = CCVector.print pp_x out v
+let pp_ro_vector pp_x out (v: _ CCVector.ro_vector) = CCVector.print pp_x out v
 let pp_klist (ppx:Format.formatter -> 'a -> unit) out l = CCKList.print ppx out l
 
 let () =
   install_printers
     [ "CCHashtbl.print"
-    ; "Containers_top.pp_vector"
+    ; "Containers_top.pp_rw_vector"
+    ; "Containers_top.pp_ro_vector"
     ; "CCBV.print"
     ; "CCDeque.print"
     ; "CCFQueue.print"
