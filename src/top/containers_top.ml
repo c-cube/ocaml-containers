5e7cfed9b826abd023524548405b56420dfc47d4
Simon Cruanes <simon.cruanes.2007@m4x.org>
2017-01-26 15:50:18 +0100

    containers.top: remove printers on structural types (#71)

--- src/top/containers_top.ml
+++ src/top/containers_top.ml
@@ -15,22 +15,15 @@ let install_printer s =
     ()
 let install_printers = List.iter install_printer
 
-let pp_rw_vector pp_x out (v: _ CCVector.vector) = CCVector.pp pp_x out v
-let pp_ro_vector pp_x out (v: _ CCVector.ro_vector) = CCVector.pp pp_x out v
-let pp_klist (ppx:Format.formatter -> 'a -> unit) out l = CCKList.pp ppx out l
-
 let () =
   install_printers
     [ "CCHashtbl.pp"
-    ; "Containers_top.pp_rw_vector"
-    ; "Containers_top.pp_ro_vector"
     ; "CCBV.pp"
     ; "CCDeque.pp"
     ; "CCFQueue.pp"
     ; "CCIntMap.pp"
     ; "CCPersistentArray.pp"
     ; "CCBigstring.pp"
-    ; "Containers_top.pp_klist"
     ; "CCKTree.pp"
     ; "CCSexpM.pp"
     ]
