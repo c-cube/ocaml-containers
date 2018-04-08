fac9f180315cf439016423e017eb6c8dc445172a
Simon Cruanes <simon.cruanes.2007@m4x.org>
2018-04-08 18:54:40 -0500

    test: consistency `CCList{,Labels}`

--- src/core/CCListLabels.ml
+++ src/core/CCListLabels.ml
@@ -2,3 +2,17 @@
 (* This file is free software, part of containers. See file "license" for more details. *)
 
 include CCList
+
+(*$inject
+  module type L = module type of CCList
+  module type LL = module type of CCListLabels
+*)
+
+(*$R
+  ignore (module CCListLabels : L)
+*)
+
+(*$R
+  ignore (module CCList : LL)
+*)
+
