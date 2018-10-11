be050473c117f18540baa3416dbc573e75f1f92f
Christopher Zimmermann <madroach@gmerlin.de>
2018-10-11 17:38:54 +0200

    Create CCStringLabels
    CAVE: there is a breaking interface change for String.is_sub

--- src/core/CCArrayLabels.ml
+++ src/core/CCArrayLabels.ml
@@ -1,21 +1,4 @@
 
 (* This file is free software, part of containers. See file "license" for more details. *)
 
-(* fallback function *)
-external make_float : int -> float array = "caml_make_float_vect"
-
 include CCArray
-
-(*$inject
-  module type A = module type of CCArray
-  module type AL = module type of CCArrayLabels
-*)
-
-(*$R
-  ignore (module CCArrayLabels : A)
-*)
-
-(*$R
-  ignore (module CCArray : AL)
-*)
-
