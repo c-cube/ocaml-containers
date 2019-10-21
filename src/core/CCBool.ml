70d7dd234d18de1ac3d3cf48d0b239740d81f3a4
Simon Cruanes <simon.cruanes.2007@m4x.org>
2019-10-21 10:34:26 -0500

    add `CCBool.{to,of}_int`

--- src/core/CCBool.ml
+++ src/core/CCBool.ml
@@ -1,4 +1,3 @@
-
 (* This file is free software, part of containers. See file "license" for more details. *)
 
 open CCShims_
@@ -8,6 +7,23 @@ type t = bool
 let equal (a:bool) b = Stdlib.(=) a b
 
 let compare (a:bool) b = Stdlib.compare a b
+
+let to_int (x:bool) : int = if x then 1 else 0
+
+(*$=
+  1 (to_int true)
+  0 (to_int false)
+*)
+
+let of_int x : t = x<>0
+
+(*$=
+  true (of_int 1)
+  false (of_int 0)
+  true (of_int 42)
+  true (of_int max_int)
+  true (of_int min_int)
+*)
 
 let negate = not
 
--- src/core/CCBool.mli
+++ src/core/CCBool.mli
@@ -1,4 +1,3 @@
-
 (* This file is free software, part of containers. See file "license" for more details. *)
 
 (** {1 Basic Bool functions} *)
@@ -9,6 +8,14 @@ val compare : t -> t -> int
 (** Total ordering on booleans, similar to {!Pervasives.compare}. *)
 
 val equal : t -> t -> bool
+
+val to_int : t -> int
+(** [to_int true = 1], [to_int false = 0].
+    @since NEXT_RELEASE *)
+
+val of_int : int -> t
+(** [of_int i] is the same as [i <> 0]
+    @since NEXT_RELEASE *)
 
 val negate : t -> t
 (** Negation on booleans (functional version of [not]).
