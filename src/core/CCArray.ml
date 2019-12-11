62672fdbd2af061ec0e7f2c3c99cf545ec985aaf
Simon Cruanes <simon.cruanes.2007@m4x.org>
2019-12-10 21:52:57 -0600

    fix test

--- src/core/CCArray.ml
+++ src/core/CCArray.ml
@@ -729,7 +729,7 @@ include Infix
 
 (* test consistency of interfaces *)
 (*$inject
-  module FA = CCShims_.Stdlib.Array.Floatarray
+  module FA = Array.Floatarray
   module type L = module type of CCArray with module Floatarray := FA
   module type LL = module type of CCArrayLabels with module Floatarray := FA
 *)
