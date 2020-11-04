87a9937938bced85524812878f0e5a7a87fbd272
Simon Cruanes <simon.cruanes.2007@m4x.org>
2020-11-04 12:29:15 -0500

    doc: indicate that ccmutheap is experimental

--- src/data/CCMutHeap.mli
+++ src/data/CCMutHeap.mli
@@ -1,3 +1,15 @@
+(* This code is extracted from Msat ( https://github.com/Gbury/mSAT ).
+   As such it is under the Apache 2 License.
+*)
+
+(** {1 Mutable Heaps}
+
+    The classic binary heap in a vector.
+
+    {b STATUS}: experimental, this might change in breaking ways.
+
+    @since NEXT_RELEASE *)
+
 module type RANKED = CCMutHeap_intf.RANKED
 
 module type S = CCMutHeap_intf.S
