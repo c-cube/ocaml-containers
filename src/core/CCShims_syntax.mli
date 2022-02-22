558c069b7bb5213e6416a348519b4b93ce34e169
Simon Cruanes <simon.cruanes.2007@m4x.org>
2022-02-21 22:03:40 -0500

    forgotten module

---
+++ src/core/CCShims_syntax.mli
@@ -0,0 +1,19 @@
+
+[@@@ifge 4.8]
+
+(** Let operators on OCaml >= 4.08.0, nothing otherwise
+  @since 2.8
+  @inline *)
+module type LET = sig
+  type 'a t
+
+  val (let+) : 'a t -> ('a -> 'b) -> 'b t
+
+  val (and+) : 'a t -> 'b t -> ('a * 'b) t
+
+  val (let*) : 'a t -> ('a -> 'b t) -> 'b t
+
+  val (and*) : 'a t -> 'b t -> ('a * 'b) t
+end
+
+[@@@endif]
