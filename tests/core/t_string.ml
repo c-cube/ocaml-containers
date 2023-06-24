ad10cdc9d5832cbc0007229baf70f652255f27c5
Simon Cruanes <simon.cruanes.2007@m4x.org>
2023-06-24 16:05:49 -0400

    fix test

--- tests/core/t_string.ml
+++ tests/core/t_string.ml
@@ -1,6 +1,7 @@
 module T = (val Containers_testlib.make ~__FILE__ ())
 include T
-open CCString.Stdlib;;
+open CCString
+open Stdlib;;
 
 q Q.printable_string (fun s -> s = rev (rev s));;
 q Q.printable_string (fun s -> length s = length (rev s));;
