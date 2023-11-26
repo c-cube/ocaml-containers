77bfa34355c1f3b99eb327de49daeaec81425c27
Master Builder <gylfi@stebbi.is>
2023-11-26 19:17:48 +0000

    CCBool: Add functions if_then and if_then_else

--- src/core/CCBool.ml
+++ src/core/CCBool.ml
@@ -1,11 +1,21 @@
 (* This file is free software, part of containers. See file "license" for more details. *)
 
-
-
 type t = bool
 
 let equal (a : bool) b = Stdlib.( = ) a b
 let compare (a : bool) b = Stdlib.compare a b
+
+let if_then f x =
+  if x then
+    Some (f ())
+  else
+    None
+
+let if_then_else f g x =
+  if x then
+    f ()
+  else
+    g ()
 
 let to_int (x : bool) : int =
   if x then
