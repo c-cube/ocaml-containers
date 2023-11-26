d1abe172cf9401c3ec68eee8c793678c5507ce62
Master Builder <gylfi@stebbi.is>
2023-11-26 19:17:48 +0000

    CCBool: Add functions if_then and if_then_else

--- src/core/CCBool.mli
+++ src/core/CCBool.mli
@@ -10,6 +10,14 @@ val compare : t -> t -> int
 val equal : t -> t -> bool
 (** [equal b1 b2] is [true] if [b1] and [b2] are the same. *)
 
+val if_then : (unit -> 'a) -> t -> 'a option
+(** [if_then f x] is [Some (f ())] if [x] is true and None otherwise.
+    @since NEXT_RELEASE *)
+
+val if_then_else : (unit -> 'a) -> (unit -> 'a) -> t -> 'a
+(** [if_then_else f g x] is [f ()] if [x] is true and [g ()] otherwise.
+    @since NEXT_RELEASE *)
+
 val to_int : t -> int
 (** [to_int true = 1], [to_int false = 0].
     @since 2.7 *)
--- tests/core/t_bool.ml
+++ tests/core/t_bool.ml
@@ -8,4 +8,8 @@ eq true (of_int 1);;
 eq false (of_int 0);;
 eq true (of_int 42);;
 eq true (of_int max_int);;
-eq true (of_int min_int)
+eq true (of_int min_int);;
+eq (Some "true") (if_then (Fun.const "true") true);;
+eq None (if_then (Fun.const "true") false);;
+eq "true" (if_then_else (Fun.const "true") (Fun.const "false") true);;
+eq "false" (if_then_else (Fun.const "true") (Fun.const "false") false)
